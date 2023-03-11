6638cda506ad88b6e026ea777091bee7986f8b47
Mochimochimarimo <99326306+Mochimochimarimo@users.noreply.github.com>
2023-03-11 20:36:41 +0900

    [add]コンテンツの追加
    コンテンツ部分のスライドの追加

---
+++ index.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
