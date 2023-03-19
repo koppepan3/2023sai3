b5079609f17377a16f715ca3416dd5cf429b0c89
github-haruki <64785351+github-haruki@users.noreply.github.com>
2023-03-19 11:27:55 +0900

    [add][mobile]swiper,記事のflexモバイル版に最適化

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
+    slidesPerView: 1.3,
     centeredSlides: false,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    spaceBetween: 20,
+    
     breakpoints: {
-      // 600px以上の場合
+      // 1200px以上の場合
       1200: {
         slidesPerView: 4
       },
       // 600px以上の場合
       600: {
-        slidesPerView: 3
+        slidesPerView: 3,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        spaceBetween: 30,
       }
     },
   });
