8b13b3010d4989b551c9b0fb974521aed3249df1
github-haruki <64785351+github-haruki@users.noreply.github.com>
2023-03-22 01:10:15 +0900

    [fix]swiperの矢印指定修正
    breakpointsでPCのみ表示にしようとしたら無理だったので修正

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1.3,
     centeredSlides: false,
     spaceBetween: 20,
-    
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
+       // 600px以上の場合
+       600: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
       // 1200px以上の場合
       1200: {
         slidesPerView: 4
-      },
-      // 600px以上の場合
-      600: {
-        slidesPerView: 3,
-        pagination: {
-          el: ".swiper-pagination",
-          type: "fraction",
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        spaceBetween: 30,
       }
+
     },
   });
