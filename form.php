8f4e6f48beb58d3d059f3b843a67f581cbd5f7a9
Mochimochimarimo <99326306+Mochimochimarimo@users.noreply.github.com>
2023-05-20 18:40:53 +0900

    [add]投票ページ

---
+++ form.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>投票</title>
+        <meta name="description" content="2023年度浜松西高等学校・同中等部文化祭「彩西祭」公式ウェブサイトへようこそ。当日の情報から準備の様子まで、様々なことを発信していきます。" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+        <link rel="stylesheet" href="css/style.css">
+        <!-- Favicon設定-->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link type="image/x-icon" rel="icon" href="favicon.ico">
+        <!-- Googlebot-->
+        <meta name="robots" content="noindex">
+        <!-- OG image settings-->
+        <meta property="og:description" content="2023年度浜松西高等学校・同中等部文化祭「彩西祭」公式ウェブサイトへようこそ。当日の情報から準備の様子まで、様々なことを発信していきます。">
+        <meta property="og:image" content="file/og_image.png">
+        <!--Google Fonts読み込み-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Zen+Kaku+Gothic+Antique:wght@500;700&family=Zen+Kaku+Gothic+New:wght@500;700&display=swap"
+            rel="stylesheet">
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V9BGKPTSGR"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-V9BGKPTSGR');
+        </script>
+    </head>
+    <body>
+        <header>
+            <a href="index.html"><img id="header_icon" src="files/header_icon.png"></a>
+            <!--  ハンバーガーボタン  -->
+            <div class="hamburger"><span></span><span></span><span></span></div>
+            <nav class="hamburger_list">
+                <div id="header_top" class="header_menu"><a href="index.html" class="header_menu"><h1>トップページ</h1></a></div>
+                <div id="header_contents" class="header_menu toggle_menu"><a href="index.html#section_contents" class="header_menu"><h1>コンテンツ</h1></a><div class="subMenu"><span></span><span></span></div></div>
+                <a class="menu_open" href="pamphlet.html"><h3>デジタル版パンフレット</h3></a>
+                <a class="menu_open" href="themeillust.html"><h3>テーマイラスト紹介</h3></a>
+                <a class="menu_open" href="map.html"><h3>校内マップ</h3></a>
+                <a class="menu_open" href="tenji.html"><h3>展示内容紹介</h3></a>
+                <div id="header_articles" class="header_menu"><a href="articles.html"><h1>記事一覧</h1></a></div>
+                <div id="header_about" class="header_menu"><a href="about.html" class="header_menu"><h1>彩西祭について</h1></a></div>
+            </nav>
+            <div class="circle-bg"></div>
+        </header>
+        <h1>投票</h1>
+        <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScoQ9qllKNb7BU6SHNCXMxxlj4cZ20PQBQ2O1FxU5BddVed6Q/formResponse" method="post">
+            <div id="question1">
+                <h1>当てはまるものを選んでください</h1>
+                <input type="radio" name="entry.1503197076">中等部生<br>
+                <input type="radio" name="entry.1503197076">高校生<br>
+                <input type="radio" name="entry.1503197076">一般の方<br>
+            </div>
+            <div id="question2">
+                <h1>所属するHRを選んでください</h1>
+                <input type="radio" name="entry.538058966">1AHR<br>
+                <input type="radio" name="entry.538058966">1BHR<br>
+                <input type="radio" name="entry.538058966">1CHR<br>
+                <input type="radio" name="entry.538058966">1DHR<br>
+                <input type="radio" name="entry.538058966">2AHR<br>
+                <input type="radio" name="entry.538058966">2BHR<br>
+                <input type="radio" name="entry.538058966">2CHR<br>
+                <input type="radio" name="entry.538058966">2DHR<br>
+                <input type="radio" name="entry.538058966">3AHR<br>
+                <input type="radio" name="entry.538058966">3BHR<br>
+                <input type="radio" name="entry.538058966">3CHR<br>
+                <input type="radio" name="entry.538058966">3DHR<br>
+                <input type="radio" name="entry.538058966">11HR<br>
+                <input type="radio" name="entry.538058966">12HR<br>
+                <input type="radio" name="entry.538058966">13HR<br>
+                <input type="radio" name="entry.538058966">14HR<br>
+                <input type="radio" name="entry.538058966">15HR<br>
+                <input type="radio" name="entry.538058966">16HR<br>
+                <input type="radio" name="entry.538058966">17HR<br>
+                <input type="radio" name="entry.538058966">21HR<br>
+                <input type="radio" name="entry.538058966">22HR<br>
+                <input type="radio" name="entry.538058966">23HR<br>
+                <input type="radio" name="entry.538058966">24HR<br>
+                <input type="radio" name="entry.538058966">25HR<br>
+                <input type="radio" name="entry.538058966">26HR<br>
+                <input type="radio" name="entry.538058966">31HR<br>
+                <input type="radio" name="entry.538058966">32HR<br>
+                <input type="radio" name="entry.538058966">33HR<br>
+                <input type="radio" name="entry.538058966">34HR<br>
+                <input type="radio" name="entry.538058966">35HR<br>
+                <input type="radio" name="entry.538058966">36HR<br>
+            </div>
+            <div id="question3">
+                <h1>所属する部活動を選んでください</h1>
+                <input type="radio" name="entry.290451178">科学研究部自然化学班<br>
+                <input type="radio" name="entry.290451178">科学研究部工学情報班<br>
+                <input type="radio" name="entry.290451178">国際文化部<br>
+                <input type="radio" name="entry.290451178">演劇部<br>
+                <input type="radio" name="entry.290451178">美術写真部美術班<br>
+                <input type="radio" name="entry.290451178">美術写真部写真班<br>
+                <input type="radio" name="entry.290451178">吹奏楽部<br>
+                <input type="radio" name="entry.290451178">弦楽部<br>
+                <input type="radio" name="entry.290451178">百人一首部<br>
+                <input type="radio" name="entry.290451178">生活文化部<br>
+                <input type="radio" name="entry.290451178">その他<br>
+            </div>
+            <div id="question4">
+                <h1>HR展賞<br>入場した展示を0点～2点の中で評価してください</h1>
+                <h2>0点</h2>
+                <h2>1点</h2>
+                <h2>2点</h2>
+                <div id="11HR">
+                    <label for="">11HR</label>
+                    <input type="radio" name="entry.885215657">
+                    <input type="radio" name="entry.885215657">
+                    <input type="radio" name="entry.885215657">
+                </div>
+                <div id="12HR">
+                    <label for="">12HR</label>
+                    <input type="radio" name="entry.1150789038">
+                    <input type="radio" name="entry.1150789038">
+                    <input type="radio" name="entry.1150789038">
+                </div>
+                <div id="13HR">
+                    <label for="">13HR</label>
+                    <input type="radio" name="entry.1923943701">
+                    <input type="radio" name="entry.1923943701">
+                    <input type="radio" name="entry.1923943701">
+                </div>
+                <div id="14HR">
+                    <label for="">14HR</label>
+                    <input type="radio" name="entry.755345731">
+                    <input type="radio" name="entry.755345731">
+                    <input type="radio" name="entry.755345731">
+                </div>
+                <div id="15HR">
+                    <label for="">15HR</label>
+                    <input type="radio" name="entry.72649444">
+                    <input type="radio" name="entry.72649444">
+                    <input type="radio" name="entry.72649444">
+                </div>
+                <div id="16HR">
+                    <label for="">16HR</label>
+                    <input type="radio" name="entry.797552058">
+                    <input type="radio" name="entry.797552058">
+                    <input type="radio" name="entry.797552058">
+                </div>
+                <div id="17HR">
+                    <label for="">17HR</label>
+                    <input type="radio" name="entry.36314777">
+                    <input type="radio" name="entry.36314777">
+                    <input type="radio" name="entry.36314777">
+                </div>
+                <div id="21HR">
+                    <label for="">21HR</label>
+                    <input type="radio" name="entry.1356478900">
+                    <input type="radio" name="entry.1356478900">
+                    <input type="radio" name="entry.1356478900">
+                </div>
+                <div id="22HR">
+                    <label for="">22HR</label>
+                    <input type="radio" name="entry.1159793844">
+                    <input type="radio" name="entry.1159793844">
+                    <input type="radio" name="entry.1159793844">
+                </div>
+                <div id="23HR">
+                    <label for="">23HR</label>
+                    <input type="radio" name="entry.1697085817">
+                    <input type="radio" name="entry.1697085817">
+                    <input type="radio" name="entry.1697085817">
+                </div>
+                <div id="24HR">
+                    <label for="">24HR</label>
+                    <input type="radio" name="entry.1735837588">
+                    <input type="radio" name="entry.1735837588">
+                    <input type="radio" name="entry.1735837588">
+                </div>
+                <div id="25HR">
+                    <label for="">25HR</label>
+                    <input type="radio" name="entry.593853303">
+                    <input type="radio" name="entry.593853303">
+                    <input type="radio" name="entry.593853303">
+                </div>
+                <div id="26HR">
+                    <label for="">26HR</label>
+                    <input type="radio" name="entry.177526831">
+                    <input type="radio" name="entry.177526831">
+                    <input type="radio" name="entry.177526831">
+                </div>
+                <div id="31HR">
+                    <label for="">31HR</label>
+                    <input type="radio" name="entry.1368722233">
+                    <input type="radio" name="entry.1368722233">
+                    <input type="radio" name="entry.1368722233">
+                </div>
+                <div id="32HR">
+                    <label for="">32HR</label>
+                    <input type="radio" name="entry.1670704581">
+                    <input type="radio" name="entry.1670704581">
+                    <input type="radio" name="entry.1670704581">
+                </div>
+                <div id="33HR">
+                    <label for="">33HR</label>
+                    <input type="radio" name="entry.1483849224">
+                    <input type="radio" name="entry.1483849224">
+                    <input type="radio" name="entry.1483849224">
+                </div>
+                <div id="34HR">
+                    <label for="">34HR</label>
+                    <input type="radio" name="entry.42765907">
+                    <input type="radio" name="entry.42765907">
+                    <input type="radio" name="entry.42765907">
+                </div>
+                <div id="35HR">
+                    <label for="">35HR</label>
+                    <input type="radio" name="entry.614844642">
+                    <input type="radio" name="entry.614844642">
+                    <input type="radio" name="entry.614844642">
+                </div>
+                <div id="36HR">
+                    <label for="">36HR</label>
+                    <input type="radio" name="entry.1290732062">
+                    <input type="radio" name="entry.1290732062">
+                    <input type="radio" name="entry.1290732062">
+                </div>
+            </div>
+            <div id="question5">
+                <h1>クラT賞<br>クラスTシャツのデザインで良いと思ったHRを１つ選んでください</h1>
+                <input type="radio" name="entry.1755872211">11HR<br>
+                <input type="radio" name="entry.1755872211">12HR<br>
+                <input type="radio" name="entry.1755872211">13HR<br>
+                <input type="radio" name="entry.1755872211">14HR<br>
+                <input type="radio" name="entry.1755872211">15HR<br>
+                <input type="radio" name="entry.1755872211">16HR<br>
+                <input type="radio" name="entry.1755872211">17HR<br>
+                <input type="radio" name="entry.1755872211">21HR<br>
+                <input type="radio" name="entry.1755872211">22HR<br>
+                <input type="radio" name="entry.1755872211">23HR<br>
+                <input type="radio" name="entry.1755872211">24HR<br>
+                <input type="radio" name="entry.1755872211">25HR<br>
+                <input type="radio" name="entry.1755872211">26HR<br>
+                <input type="radio" name="entry.1755872211">31HR<br>
+                <input type="radio" name="entry.1755872211">32HR<br>
+                <input type="radio" name="entry.1755872211">33HR<br>
+                <input type="radio" name="entry.1755872211">34HR<br>
+                <input type="radio" name="entry.1755872211">35HR<br>
+                <input type="radio" name="entry.1755872211">36HR<br>
+                <input type="radio" name="entry.1755872211">投票しない<br>
+            </div>
+            <div id="question6">
+                <h1>ベストコーディネート賞<br>異装のデザインで良いと思ったHR・部活動を１つ選んでください。</h1>
+                <input type="radio" name="entry.401001952">11HR<br>
+                <input type="radio" name="entry.401001952">12HR<br>
+                <input type="radio" name="entry.401001952">13HR<br>
+                <input type="radio" name="entry.401001952">14HR<br>
+                <input type="radio" name="entry.401001952">15HR<br>
+                <input type="radio" name="entry.401001952">16HR<br>
+                <input type="radio" name="entry.401001952">17HR<br>
+                <input type="radio" name="entry.401001952">21HR<br>
+                <input type="radio" name="entry.401001952">22HR<br>
+                <input type="radio" name="entry.401001952">23HR<br>
+                <input type="radio" name="entry.401001952">24HR<br>
+                <input type="radio" name="entry.401001952">25HR<br>
+                <input type="radio" name="entry.401001952">26HR<br>
+                <input type="radio" name="entry.401001952">31HR<br>
+                <input type="radio" name="entry.401001952">32HR<br>
+                <input type="radio" name="entry.401001952">33HR<br>
+                <input type="radio" name="entry.401001952">34HR<br>
+                <input type="radio" name="entry.401001952">35HR<br>
+                <input type="radio" name="entry.401001952">36HR<br>
+                <input type="radio" name="entry.401001952">投票しない<br>
+            </div>
+            <button type="submit">送信</button>
+        </form>
+        <footer>
+            <div class="footer_contents">
+                <div class="footer_icon"><img src="files/header_icon.svg" alt=""></div>
+                <div class="footer_items" id="footer_items_1">
+                    <a href="about.html"><img src="files/footer_about.svg" alt=""></a>
+                    <a href="map.html"><img src="files/footer_map.svg" alt=""></a>
+                    <a href="timetable.html"><img src="files/footer_timetable.svg" alt=""></a>
+                </div>
+                <div class="footer_items" id="footer_items_2">
+                    <a href="disply.html"><img src="files/footer_display.svg" alt=""></a>
+                    <a href="http://www.edu.pref.shizuoka.jp/hamamatsunishi-h/home.nsf/IndexFormView?OpenView"><img src="files/footer_HP.svg" alt=""></a>
+                </div>
+                <div class="school_addres"><img src="files/school_addres.svg" alt=""></div>
+            </div>
+        </footer>
+        <!--jQuery読み込み-->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <!--Hamburger buttond JS-->
+        <script>
+            $(function() {
+                $('.hamburger').click(function() {
+                    $(this).toggleClass('active');
+                    
+                    if ($(this).hasClass('active')) {
+                        $('.hamburger_list').addClass('active');
+                        $(".circle-bg").addClass('circleactive');//丸背景にcircleactiveクラスを付与
+                    } else {
+                        $('.hamburger_list').removeClass('active');
+                        $(".circle-bg").removeClass('circleactive');//丸背景にcircleactiveクラスを付与
+                    }
+                });
+
+                $('.subMenu').click(function() {
+                    $(this).toggleClass('active');
+                    
+                    if ($(this).hasClass('active')) {
+                        $('.subMenu').addClass('active');
+                        $('.menu_open').addClass('active');
+                    } else {
+                        $('.subMenu').removeClass('active');
+                        $('.menu_open').removeClass('active');
+                    }
+                });
+
+            });
+        </script>
+        <!--EmbedPost読み込み JS-->
+        <script>
+            $(function(){
+                $("#articles_holder").load("latestposts.html");
+            });
+        </script>
+    </body>
+</html>
